97ff083e1fee7e3ea2d8e046ed56c99ebf92efe5
TEMMAR Mohamed Amine <temmaramine930@gmail.com>
2023-04-11 00:26:27 +0000

    aut

--- free-trading/src/component/AutoComplete.jsx
+++ free-trading/src/component/AutoComplete.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import finnhub from '../api/finnhub';
 
 const AutoComplete = () => {
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await finnhub.get("/search", {
+          params:{
+            q: search
+          }
+        })
+        console.log(response);
+      } catch (error) {
+        
+      }
+    }
+    if ( search.length > 0 ) {
+      fetchData();
+    }
+  })
+
   return (
-    <div>AutoComplete</div>
+    <div className='w-50 p-5 rounded mx-auto'>
+      <div className='form-floating dropdown'>
+
+        <input style={{ backgroundColor: "rgba(145, 158, 171, 0.04)"}} id='search' className='form-control' type="text" placeholder='Search' autoComplete='off' value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <label htmlFor="search">Search</label>
+        <ul className='dropdown-menu show'>
+          <li>Stock1</li>
+          <li>Stock2</li>
+          <li>Stock3</li>
+        </ul>
+
+      </div>
+    </div>
   )
 }
 
