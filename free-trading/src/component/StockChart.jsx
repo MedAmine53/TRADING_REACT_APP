4814b8f1965fda02ee05ab78d8ca5762d4c0cbf5
TEMMAR Mohamed Amine <temmaramine930@gmail.com>
2023-04-16 22:22:23 +0000

    delete stock

--- free-trading/src/component/StockChart.jsx
+++ free-trading/src/component/StockChart.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import Chart from "react-apexcharts";
 
 export const StockChart = ({chartData, symbol}) => {
+  const [dateFormat, setDateFormat] = useState("24h")
   const {day, week, year} = chartData
+
+  const determineTimeFormat = () => {
+    switch(dateFormat) {
+      case "24h":
+        return day
+      case "7d":
+        return week
+      case "1y": 
+        return year
+      default:
+        return day
+    }
+  }
+
+  const color = determineTimeFormat()[determineTimeFormat().length -1].y - determineTimeFormat()[0].y > 0 ? "#26c281" : "ed3419"
+
   const options = {
+    color: [color],
     title: {
       text: symbol,
       align: "center",
@@ -30,12 +49,27 @@ export const StockChart = ({chartData, symbol}) => {
       }
     }
   }
+
   const series = [{
     name: symbol,
-    data: day
+    data: determineTimeFormat()
   }]
+
+  const renderButtonSelect = (button) => {
+    const classes = "btn m-1"
+    if(button === dateFormat){
+      return classes + "btn-primary"
+    }else{
+      return classes + "btn-outline-primary"
+    }
+  }
 
   return <div className="mt-5 p-4 shadow-sm bg-white">
           <Chart options={options} series={series} type="area" width="100%" />
+          <div>
+            <button className={renderButtonSelect("24h")} onClick={() => setDateFormat("24h")}>24h</button>
+            <button className={renderButtonSelect("7d")} onClick={() => setDateFormat("7d")}>7d</button>
+            <button className={renderButtonSelect("1y")} onClick={() => setDateFormat("1y")}>1y</button>
+          </div>
         </div>
 }
--- free-trading/src/component/StockList.jsx
+++ free-trading/src/component/StockList.jsx
@@ -84,7 +84,10 @@ export const StockList = () => {
               <td>{stockData.data.h}</td>
               <td>{stockData.data.l}</td>
               <td>{stockData.data.o}</td>
-              <td>{stockData.data.pc}</td>
+              <td>{stockData.data.pc}  <button className='btn btn-danger btn-sm ml-3 d-inline-block delete-button' onClick={(e) => {
+                e.stopPropagation()
+                deleteStock(stockData.symbol)
+              }}>Remove</button></td>
             </tr>
           )
         })}
